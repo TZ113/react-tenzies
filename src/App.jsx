e22ffebf3d07a594d20ee98f26627d3b949f9a29
TZ113 <earnestlearneran@gmail.com>
2023-10-17 23:54:10 +0600

    adding features

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,27 @@
 
 import { nanoid } from "nanoid"
 import { useEffect, useRef, useState } from "react"
+import Modal from "react-modal"
 import { Die, RunConfetti } from "./Components"
+
+Modal.setAppElement("#root")
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    border: "2px groove dodgerblue",
+    maxWidth: "500px",
+    width: "100%",
+    borderRadius: "7px",
+    textAlign: "center",
+    backgroundColor: "whitesmoke",
+  },
+}
 
 function App() {
 
@@ -10,7 +30,10 @@ function App() {
   const [tenzies, setTenzies] = useState(false)
   const [clickCount, setClickCount] = useState(0)
   const [records, setRecords] = useState(() => JSON.parse(localStorage.getItem("records")) || [])
+  const [modalIsOpen, setModalIsOpen] = useState(false)
   const startTimeRef = useRef(null)
+
+  console.log(records)
 
   useEffect(() => {
     const firstValue = dice[0].value
@@ -55,11 +78,10 @@ function App() {
   }
 
   function holdDie(id) {
-    console.log(id)
     setDice(prevDice => prevDice.map(die =>
       die.id === id ? { ...die, held: !die.held } : die
     ))
-    setClickCount(prevCount => prevCount + 1)
+    // setClickCount(prevCount => prevCount + 1)
   }
 
   function rollDice() {
@@ -72,6 +94,7 @@ function App() {
       setTenzies(false)
       setDice(AllNewDice())
       setClickCount(0)
+      // setModalIsOpen(true)
     }
   }
 
@@ -81,6 +104,17 @@ function App() {
 
   return (
     <main>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        className="customModal"
+        overlayClassName="customOverlay"
+        contentLabel="Stats"
+      >
+        <h1 className="modal-heading">Scores</h1>
+        <p className="modal-content">Modal content</p>
+      </Modal>
+      {/* <button onClick={() => setModalIsOpen(true)}>Open Modal</button> */}
       {tenzies && <RunConfetti />}
       <h1>TENZI Dice Game</h1>
       <h3>Press Roll to roll the dice. Click on a die to hold it. If you get all the dice same, you have won!</h3>
